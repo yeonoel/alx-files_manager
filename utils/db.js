cd46c8c28060526a00aee66d297120b7b71b4a05
yeonoel <yeopevroguinoel@gmail.com>
2023-03-08 22:20:54 +0000

    mongodb utils

--- utils/db.js
+++ utils/db.js
@@ -3,14 +3,13 @@ import mongodb from 'mongodb';
 class DBClient {
   constructor() {
     const host = process.env.DB_HOST || 'localhost';
-    const port = process.env.DB_PORT ||  27017;
+    const port = process.env.DB_PORT || 27017;
     const database = process.env.DB_DATABASE || 'files_manager';
-    
+
     const url = `mongodb://${host}:${port}/${database}`;
     this.client = new mongodb.MongoClient(url);
-    
-    this.client.connect();
 
+    this.client.connect();
   }
 
   isAlive() {
