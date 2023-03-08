e2510d66de3cf5f06865ee83b9e71ab36bac0199
yeonoel <yeopevroguinoel@gmail.com>
2023-03-08 15:31:52 +0000

    Update

--- utils/redis.js
+++ utils/redis.js
@@ -1,9 +1,6 @@
 import { promisify } from 'util';
 import { createClient } from 'redis';
 
-/**
- * Represents a Redis client.
- */
 class RedisClient {
   /**
    * Creates a new RedisClient instance.
@@ -37,23 +34,11 @@ class RedisClient {
     return promisify(this.client.GET).bind(this.client)(key);
   }
 
-  /**
-   * Stores a key and its value along with an expiration time.
-   * @param {String} key The key of the item to store.
-   * @param {String | Number | Boolean} value The item to store.
-   * @param {Number} duration The expiration time of the item in seconds.
-   * @returns {Promise<void>}
-   */
   async set(key, value, duration) {
     await promisify(this.client.SETEX)
       .bind(this.client)(key, duration, value);
   }
 
-  /**
-   * Removes the value of a given key.
-   * @param {String} key The key of the item to remove.
-   * @returns {Promise<void>}
-   */
   async del(key) {
     await promisify(this.client.DEL).bind(this.client)(key);
   }
