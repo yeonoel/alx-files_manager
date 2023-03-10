0ea48c1f4f9806b39c3f4ce496acf83d3185da51
yeonoel <yeopevroguinoel@gmail.com>
2023-03-10 19:13:04 +0000

    Update

--- controllers/AppController.js
+++ controllers/AppController.js
@@ -6,14 +6,14 @@ export default class AppController {
   static getStatus(req, res) {
     res.status(200).json({
       redis: redisClient.isAlive(),
-      db: dbClient.isAlive(),
+      db: dbClient.isAlive()
     });
   }
 
   static getStats(req, res) {
     Promise.all([dbClient.nbUsers(), dbClient.nbFiles()])
-	  .then(([usersCount, filesCount]) => {
-            res.status(200).json({ users: usersCount, files: filesCount })
-	  });
-    };
-};
+      .then(([usersCount, filesCount]) => {
+        res.status(200).json({ users: usersCount, files: filesCount });
+      });
+  }
+}
--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,8 @@
 import AppController from '../controllers/AppController'
 
-
 const injectRoutes = (myApp) => {
-
   myApp.get('/status', AppController.getStatus);
   myApp.get('/stats', AppController.getStats);
-
-}
+};
 
 export default injectRoutes;
