254a90f4ecadba5cd172e107c86972aac20b6478
yeonoel <yeopevroguinoel@gmail.com>
2023-03-10 19:03:59 +0000

    Update

--- controllers/AppController.js
+++ controllers/AppController.js
@@ -13,7 +13,7 @@ export default class AppController {
   static getStats(req, res) {
     Promise.all([dbClient.nbUsers(), dbClient.nbFiles()])
 	  .then(([usersCount, filesCount]) => {
-            res.status(200).json({users: usersCount, files: filesCount })
+            res.status(200).json({ users: usersCount, files: filesCount })
 	  });
     };
 };
