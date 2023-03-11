15fac967b0a4868bac189d83d625ded6deb9d1a0
yeonoel <yeopevroguinoel@gmail.com>
2023-03-11 22:27:38 +0000

    Authenticate a user

--- libs/middlewares.js
+++ libs/middlewares.js
@@ -1,7 +1,7 @@
 import express from 'express';
 
 /**
- ** Adds middlewares to extract the body from POST requests.
+ * Adds middlewares to extract the body from POST requests.
  **/
 const injectMiddlewares = (myApp) => {
   myApp.use(express.json({ limit: '200mb' }));
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,15 @@
 import AppController from '../controllers/AppController';
 import UsersController from '../controllers/UsersController';
+import AuthController from '../controllers/AuthController';
+import { basicAuthenticate, xTokenAuthenticate } from '../middlewares/auth';
 
 const injectRoutes = (myApp) => {
   myApp.get('/status', AppController.getStatus);
   myApp.get('/stats', AppController.getStats);
+
+  myApp.get('/connect', basicAuthenticate, AuthController.getConnect);
+  myApp.get('/disconnect', xTokenAuthenticate, AuthController.getDisconnect);
+  myApp.get('users/me', xTokenAuthenticate, UsersController.getMe);
 
   myApp.post('/users', UsersController.postNew);
 };
