2d7cc6257c622f868954efa9fcd16e413e311147
yeonoel <yeopevroguinoel@gmail.com>
2023-03-12 15:34:47 +0000

    Authenticate a user

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -2,7 +2,7 @@
 import { v4 as uuidv4 } from 'uuid';
 import redisClient from '../utils/redis';
 
-export default class  AuthController {
+export default class AuthController {
   static async getConnect(req, res) {
     const { user } = req;
     const token = uuidv4();
@@ -14,7 +14,7 @@ export default class  AuthController {
 
   static async getDisconnect(req, res) {
     const token = req.headers['x-token'];
-    
+
     await redisClient.del(`auth_${token}`);
     res.status(204).send();
   }
