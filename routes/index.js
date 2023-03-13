4ed4dd6b931189e8d234a3516cd602d85c5f2fa4
yeonoel <yeopevroguinoel@gmail.com>
2023-03-13 00:00:31 +0000

    First file

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ import AppController from '../controllers/AppController';
 import UsersController from '../controllers/UsersController';
 import AuthController from '../controllers/AuthController';
 import { basicAuthenticate, xTokenAuthenticate } from '../middlewares/auth';
+import FilesController from '../controllers/FilesController';
 
 const injectRoutes = (myApp) => {
   myApp.get('/status', AppController.getStatus);
@@ -12,6 +13,8 @@ const injectRoutes = (myApp) => {
 
   myApp.post('/users', UsersController.postNew);
   myApp.get('/users/me', xTokenAuthenticate, UsersController.getMe);
+
+  myApp.post('/files', xTokenAuthenticate, FilesController.postUpload);
 };
 
 export default injectRoutes;
